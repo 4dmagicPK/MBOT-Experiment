bf0e158ee198f94eb7cfbc6997c984076c0e6077
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-08-04 11:34:44 +0500

    Shopping cart is completed

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,37 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
+import {ShoppingCartService} from './shopping-cart.service'
 export interface Tile {
   color: string;
   cols: number;
   rows: number;
   text: string;
 }
-
-
-
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.less']
 })
+
 export class ShoppingCartComponent {
-  
+  showDetailsContainer: boolean = false;
+  showContent:string="show Details ᐯ";
+  shoppingData:any;
+
+  @ViewChild('cardElement', { read: ElementRef }) cardElementRef!: ElementRef;
+
   tiles: Tile[] = [
     {text: 'One', cols: 1, rows: 2, color: 'lightblue'},
     {text: 'Two', cols: 3, rows: 7, color: 'lightgreen'},
     
   ];
 
+showDetails() {
+  this.showDetailsContainer = !this.showDetailsContainer;
+  this.showContent=this.showDetailsContainer?"show Details ᐱ":"show Details ᐯ"
 
+  // if (this.showDetailsContainer) {
+  //   const cardWidth = this.cardElementRef.nativeElement.getBoundingClientRect().width;
+  //   const detailsContainer = document.querySelector('.details-container') as HTMLElement;
+  //   detailsContainer.style.width = `50px`;
+  // }
+}
 
 
   isSmallScreen = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver,private shoppingCartService:ShoppingCartService) {
     this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.XSmall]).subscribe(result => {
       this.isSmallScreen = result.matches;
       this.updateTileCols();
+
+      shoppingCartService.getShoppingData().subscribe((data)=>{
+       this.shoppingData=data;
+      })
     });
   }
 
